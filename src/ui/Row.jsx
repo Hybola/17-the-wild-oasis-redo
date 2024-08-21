7cd3b13a4d80d14b3c3c8ccac543e251d1da35f7
Hybola <keeratipong.b@gmail.com>
2024-08-21 16:41:28 +0700

    lecture 333: set up routes

--- src/ui/Row.jsx
+++ src/ui/Row.jsx
@@ -19,3 +19,18 @@ Row.defaultProps = {
   type: "vertical",
 };
 export default Row;
+////Version 2
+const direction = {
+  horizontal: css`
+    justify-content: space-between;
+    align-items: center;
+  `,
+  vertical: css`
+    flex-direction: column;
+    gap: 1.6rem;
+  `,
+};
+const RowV2 = styled.div`
+  display: flex;
+  ${(props) => direction[props.type]}
+`;
