d03c7c7f6e575f0710ca567303ff20ae0b6ce32a
Hybola <keeratipong.b@gmail.com>
2024-08-28 15:56:11 +0700

    lecture 354

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,6 +5,7 @@ import Sidebar from "./Sidebar";
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+  overflow: scroll;
 `;
 const StyleAppLayout = styled.div`
   display: grid;
