2b44ca4279115a8b14b47a530a3c74326f469787
Hybola <keeratipong.b@gmail.com>
2024-09-18 16:36:12 +0700

    lecture 389: user login

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,16 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+  const navigate = useNavigate();
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: loginApi,
+    onSuccess: (data) => {
+      navigate("/dashboard");
+    },
+    onError: () => toast.error("Provided email or password are incorrect"),
+  });
+  return { login, isLoading };
+}
