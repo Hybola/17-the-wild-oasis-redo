b77c17bcebf9c7e7c55310198ffe99fa2174a4d1
Hybola <keeratipong.b@gmail.com>
2024-09-25 17:02:43 +0700

    lecuture 400: request data from API to dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +8,18 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+function DashboardLayout() {
+  const { bookings, isLoading1 } = useRecentBookings();
+  const { confirmedStays, isLoading: isLoading2 } = useRecentStays();
+  if (isLoading1 || isLoading2) return <Spinner />;
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>today</div>
+      <div>Chart Stay duration</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
